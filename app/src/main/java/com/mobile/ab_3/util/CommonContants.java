b7d320604c3263bf24b9900f83877b9ae4528f20
programmingassignmenthelper <programmingassignmentsservices@gmail.com>
2024-11-27 11:51:35 +0600

    Completed Lab 2

---
+++ app/src/main/java/com/mobile/ab_3/util/CommonContants.java
@@ -0,0 +1,9 @@
+package com.mobile.ab_3.util;
+
+/**
+ * Created by Bikash Sahani on 03/10/24.
+ */
+public class CommonContants {
+    public static final String MY_PREFERENCE = "MyPrefs";
+    public static final String NAME = "name";
+}
